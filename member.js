1ff92b8deb69edff8d3b27c8eb3c11f9fa9512c2
Shubham <51116166+spawade99@users.noreply.github.com>
2024-05-28 12:35:56 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    console.log("Skills of the member...");
+    console.log("Skill 1: Java");
+    console.log("Skill 2: Python");
+    console.log("Skill 3: JavaScript");
+}
